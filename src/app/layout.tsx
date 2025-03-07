29778236639494f79d50c0049a758a3dcc8eacd8
djdheeraj5701 <djdheeraj5701@gmail.com>
2025-03-08 01:36:26 +0530

    working app again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Mood Tracker",
   description: "Track your moods easily with Firebase",
 };
